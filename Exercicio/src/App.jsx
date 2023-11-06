294f719e1e398b49edce39d165f2be8b736dd132
S-Evangelista <stefanystrenner@gmail.com>
2023-11-06 20:54:07 -0300

    Atividades jsx feitas

--- Exercicio/src/App.jsx
+++ Exercicio/src/App.jsx
@@ -1,35 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tarefa, setTarefa] = useState('');
+  const [itensLista, setItensLista] = useState([]);
+
+  const DeliveryStatus = () => {
+    const [status, setStatus] = useState('Pendente');
+   
+    const updateStatus = (newStatus) => {
+       setStatus(newStatus);
+    }
+  }
+   
+
+  const [theme, setTheme] = useState('claro')
+
+  const handleThemeChange = () => {
+    setTheme(theme === 'claro' ? 'escuro' : 'claro');
+  }
+
+  function handleChangeInput(event) {
+    const inputTarefa = event.target.value;
+
+    setTarefa(inputTarefa);
+  }
+
+  function adicionarNaLista(event) {
+    event.preventDefault();
+
+    if (!tarefa) {
+      return;
+    }
+
+    setItensLista([...itensLista, tarefa]);
+    setTarefa('');
+  }
+
+  function excluirItem(index) {
+    const novosItens = [...itensLista];
+    novosItens.splice(index, 1);
+    setItensLista(novosItens);
+  }
+
 
   return (
-    <>
+    <div className={`theme-switcher ${theme}`}>
+
+      <h1>Selecione o tema</h1>
+      <button onClick={handleThemeChange}>
+        Tema: {theme === 'claro' ? 'escuro' : 'claro'}
+      </button>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <h1>Status da entrega: {status}</h1>
+      <button onClick={() => updateStatus('Pendente')}>Pendente</button>
+      <button onClick={() => updateStatus('Em trânsito')}>Em trânsito</button>
+      <button onClick={() => updateStatus('Entregue')}>Entregue</button>
+    </div>
+
+      <h1>Lista de tarefas</h1>
+      <form onSubmit={adicionarNaLista}>
+        <input
+          type="text"
+          placeholder="Adicione uma tarefa"
+          onChange={handleChangeInput}
+          value={tarefa}
+        />
+
+        <button type="submit"> Adicionar </button>
+      </form>
+
+      <ul className="lista">
+        {itensLista.map((item, index) => (
+          <li key={index}>{item}
+            <button onClick={() => excluirItem(index)}>Excluir</button>
+          </li>
+
+        ))}
+      </ul>
+      
+
+    </div>
+  );
 }
 
-export default App
+
+export default App;
+
+
--- Exercicio/src/main.jsx
+++ Exercicio/src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Lista from './Lista.jsx'
 import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className='fundo'>
-    <Lista/>
+    <App/>
   </div>
 )
