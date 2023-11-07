f6813c5ba61bb38b08ab755d8d3c4cb9bbcc387f
S-Evangelista <stefanystrenner@gmail.com>
2023-11-06 21:17:27 -0300

    Atualização
    Falta fazer as letras mudarem de acordo com o tema

--- Exercicio/src/App.jsx
+++ Exercicio/src/App.jsx
@@ -5,15 +5,6 @@ function App() {
   const [tarefa, setTarefa] = useState('');
   const [itensLista, setItensLista] = useState([]);
 
-  const DeliveryStatus = () => {
-    const [status, setStatus] = useState('Pendente');
-   
-    const updateStatus = (newStatus) => {
-       setStatus(newStatus);
-    }
-  }
-   
-
   const [theme, setTheme] = useState('claro')
 
   const handleThemeChange = () => {
@@ -51,13 +42,7 @@ function App() {
       <button onClick={handleThemeChange}>
         Tema: {theme === 'claro' ? 'escuro' : 'claro'}
       </button>
-      <div>
-      <h1>Status da entrega: {status}</h1>
-      <button onClick={() => updateStatus('Pendente')}>Pendente</button>
-      <button onClick={() => updateStatus('Em trânsito')}>Em trânsito</button>
-      <button onClick={() => updateStatus('Entregue')}>Entregue</button>
-    </div>
-
+      
       <h1>Lista de tarefas</h1>
       <form onSubmit={adicionarNaLista}>
         <input
--- Exercicio/src/main.jsx
+++ Exercicio/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Entrega from './Entrega.jsx'
 import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className='fundo'>
     <App/>
+    <Entrega/>
   </div>
 )
